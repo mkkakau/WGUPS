d56054b6e4bd15edf817dfa00208f7c6981781d3
Melanie A <mkkakau@gmail.com>
2021-09-26 15:44:01 -1000

    Add default note on package in constructor

--- Package.py
+++ Package.py
@@ -1,7 +1,7 @@
 class Package():
     """A class representing a Package"""
 
-    def __init__(self, id, location_id, deadline, mass, note):
+    def __init__(self, id, location_id, deadline, mass, note=""):
         self.id = id
         self.location_id = location_id
         self.deadline = deadline
