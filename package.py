4f1723662a31a2fa1e6f4ef0a55a73acbd2dfb88
Melanie A <mkkakau@gmail.com>
2021-09-27 17:41:55 -1000

    Add priorities to package class

--- package.py
+++ package.py
@@ -1,3 +1,14 @@
+from enum import Enum
+
+
+class Priority(Enum):
+    """An enumerator representing package priority"""
+
+    HIGH = 3
+    MEDIUM = 2
+    LOW = 1
+
+
 class Package():
     """A class representing a Package"""
 
@@ -7,3 +18,12 @@ class Package():
         self.deadline = deadline
         self.mass = mass
         self.note = note
+        self.priority = self._get_priority(self.deadline)
+
+    def _get_priority(self, deadline):
+        priority = Priority.LOW
+        if deadline == "9:00 AM":
+            priority = Priority.HIGH
+        if deadline == "10:30 AM":
+            priority = Priority.MEDIUM
+        return priority
