5f212cdec6f33640f3dc970073e6d95fea82c247
Melanie A <mkkakau@gmail.com>
2021-09-27 17:10:08 -1000

    Create location class

---
+++ location.py
@@ -0,0 +1,11 @@
+class Location():
+    """A class representing a location"""
+
+    def __init__(self, filename):
+        self.filename = filename
+        self.id = -1
+        self.address = ""
+        self.city = ""
+        self.state = ""
+        self.zipcode = ""
+        self.name = ""
