a3f7ae234a720e2cffa91d6595616f14fdb91e9b
Melanie A <mkkakau@gmail.com>
2021-09-28 20:17:17 -1000

    Convert deadline to proper datetime.time object

--- package.py
+++ package.py
@@ -1,4 +1,5 @@
 from enum import Enum
+from datetime import datetime
 
 
 class Priority(Enum):
@@ -15,10 +16,15 @@ class Package():
     def __init__(self, id, location_id, deadline, mass, note=""):
         self.id = id
         self.location_id = location_id
-        self.deadline = deadline
+        self.deadline = self._convert_deadline(deadline)
         self.mass = mass
         self.note = note
         self.priority = self._get_priority(self.deadline)
+
+    def _convert_deadline(self, deadline):
+        format_data = "%H:%M %p"
+        formatted_deadline = datetime.strptime(deadline, format_data).time()
+        return formatted_deadline
 
     def _get_priority(self, deadline):
         priority = Priority.LOW
