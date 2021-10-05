b6f7c8f75d58d6b6ec446ca9723ce1ee52fc86d5
Melanie A <mkkakau@gmail.com>
2021-10-04 17:15:06 -1000

    Expand truck class so that it can go to a location and calculate distance and time travelled

--- truck.py
+++ truck.py
@@ -1,7 +1,58 @@
+from datetime import timedelta, time, datetime, date
+import data
+from package import Status
+
+
 class Truck():
     """A class representing a truck"""
 
-    def __init__(self, id, packages):
+    def __init__(self, id, packages, start_time):
         self.id = id
+        self.speed = 18
         self.total_distance = 0
+        self.start_time = start_time
+        self.end_time = start_time
+        self.current_loc = 1
         self.packages = packages
+        self.path = self._calculate_path(packages)
+
+    def _calculate_path(self, packages):
+        return [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+    def add_distance(self, distance):
+        self.total_distance += distance
+
+    def add_time(self, time_change):
+        self.end_time += time_change
+
+    def _calculate_time_change(self, distance):
+        hours_elapsed = distance / self.speed
+        change = timedelta(hours=hours_elapsed)
+        return change
+
+    def go_to(self, location_id):
+        distance = data.distances.get_distance(self.current_loc, location_id)
+        self.add_distance(distance)
+        time_change = self._calculate_time_change(distance)
+        self.add_time(time_change)
+        self.current_loc = location_id
+
+    # O(2n)
+    def start(self):
+        # O(n) Set status of all packages to EN_ROUTE
+        for p in enumerate(self.packages):
+            p.status = Status.EN_ROUTE
+
+        # O(n)
+        for next in self.path:
+            self.go_to(next)
+            package = self.packages.search(next)
+            package.status = Status.DELIVERED
+            print(package)
+            print(package.status)
+
+
+t = Truck(1, data.packages.get(1), datetime.combine(
+    date.today(), time(hour=8)))
+print(t._calculate_time_change(0.6))
+t.start()
