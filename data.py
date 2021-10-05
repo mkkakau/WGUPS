e2dff8535b0f2016f7694073a06d29b847a237d8
Melanie A <mkkakau@gmail.com>
2021-10-04 17:15:55 -1000

    Create data module to store data from csv files

---
+++ data.py
@@ -0,0 +1,8 @@
+from distance import DistanceList
+from package import PackageList
+from location import LocationList
+
+
+locations = LocationList("locations.csv")
+distances = DistanceList("distances.csv")
+packages = PackageList("packages.csv")
